1319592b4dda89d759d905bd3996302b0e71487f
dsaltaren07 <dsaltaren07@gmail.com>
2025-08-27 14:54:15 -0500

    tests for saving and searching by name

--- src/test/java/com/educastro/sales/service/ProductServiceTest.java
+++ src/test/java/com/educastro/sales/service/ProductServiceTest.java
@@ -3,6 +3,7 @@ package com.educastro.sales.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -13,9 +14,11 @@ import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 
 import com.educastro.sales.exception.ResourceNotFoundException;
+import com.educastro.sales.model.dto.ProductDTO;
 import com.educastro.sales.model.entities.Product;
 import com.educastro.sales.model.mapper.ProductMapper;
 import com.educastro.sales.repository.ProductRepository;
+import org.springframework.dao.DuplicateKeyException;
 
 public class ProductServiceTest {
 
@@ -51,5 +54,56 @@ public class ProductServiceTest {
         when(productRepository.findById(99)).thenReturn(Optional.empty());
 
         assertThrows(ResourceNotFoundException.class, () -> productService.findById(99));
+    }
+
+    
+       @Test
+    void testFindByNameSuccessfully() {
+        Product product = new Product("Keyboard", 50);
+        product.setId(1);
+
+        when(productRepository.findByNameLike("keyboard")).thenReturn(Optional.of(product));
+
+        Product found = productService.findByName("keyboard");
+
+        assertNotNull(found);
+        assertEquals("Keyboard", found.getName());
+    }
+
+    @Test
+    void testFindByNameNotFoundThrowsException() {
+        when(productRepository.findByNameLike("Mouse")).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> productService.findByName("Mouse"));
+    }
+
+
+    @Test
+    void testSaveSuccessfully() {
+       ProductDTO dto = new ProductDTO();
+    Product product = new Product("keyboard", 30);
+    product.setId(1);
+
+    when(productRepository.existsByName("keyboard")).thenReturn(false);
+    when(productMapper.map(dto)).thenReturn(product);
+    when(productRepository.save(product)).thenReturn(product);
+
+    Product result = productService.save(dto);
+
+    assertNotNull(result);
+    assertEquals("keyboard", result.getName());
+    assertEquals(30, result.getPrice());
+    }
+
+
+    @Test
+    void testSaveDuplicateNameThrowsException() {
+        ProductDTO dto = new ProductDTO();
+        dto.setName("keyboard");
+
+        when(productRepository.existsByName("keyboard")).thenReturn(true);
+
+        assertThrows(DuplicateKeyException.class, () -> productService.save(dto));
+      
     }
 }
